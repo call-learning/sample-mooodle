be0335c59db0d64728fdd0139d1cb9ad106274be
Laurent David <lmedavid@gmail.com>
2022-04-05 12:22:31 +0200

    Test LDAP

---
+++ local/ldap/db/events.php
@@ -0,0 +1,27 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
+
+
+// List of events_2 observers.
+
+$observers = array(
+    array(
+        'eventname'   => 'core\event\user_loggedin',
+        'callback'    => 'local_ldap\observer::user_has_logged_in',
+    ),
+);
